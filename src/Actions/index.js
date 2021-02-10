b1e4e69baa8d856fc6f0679169a3f7e9e9a7bde3
omerulucan16 <omerulucan@windowslive.com>
2021-02-10 14:18:44 +0300

    Clear Debugger

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -2,9 +2,7 @@ import axios from "axios";
 import React from "react";
 
 export const getCountryList = () => (dispatch) => {
-  debugger;
   axios.get("https://restcountries.eu/rest/v2/region/europe").then((res) => {
-    debugger;
     dispatch({
       type: "SET_COUNTRY_LIST",
       payload: res.data,
@@ -12,7 +10,6 @@ export const getCountryList = () => (dispatch) => {
   });
 };
 export const setSelectedCountry = (code) => (dispatch) => {
-  debugger;
   dispatch({
     type: "SET_SELECTED_COUNTRY",
     payload: code,
